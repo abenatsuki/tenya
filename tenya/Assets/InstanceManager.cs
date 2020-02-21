29fb9e43d868ac7e82d5ab2647c4c8e496a63fe1
abeprogram <52775445+abeprogram@users.noreply.github.com>
2020-02-21 17:10:55 +0900

    スポーン

---
+++ tenya/Assets/InstanceManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class InstanceManager : MonoBehaviour
+{
+    [SerializeField,Tooltip("ランダムx最小値")]
+    float min;
+    [SerializeField,Tooltip("ランダムx最大値")]
+    float max;
+    [SerializeField,Tooltip("プレふぁぶリスト")]
+    List<GameObject> objects;
+
+    [SerializeField,Tooltip("生成間隔")]
+    int spownTime;
+
+    float time;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > spownTime) { 
+        Instantiate(objects[Random.Range(0, objects.Count)], new Vector3(Random.Range(min, max), transform.position.y, 0), new Quaternion(0, 0, 0, 0));
+        time = 0; }
+        
+        
+    }
+}
---
+++ tenya/Assets/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Transform explosionPrefab;
+    Rigidbody pRigidBody;
+    Collider collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pRigidBody = GetComponent<Rigidbody>();
+        collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        pRigidBody.AddForce(new Vector3(-10f, 0, 0));
+        if (Input.GetKey(KeyCode.D))
+            pRigidBody.AddForce(new Vector3(10f, 0, 0));
+
+       // collider.transform.posi = new Vector3(0,0,1);
+    }
+   
+    void OnCollisionEnter(Collision collision)
+    {
+       
+        pRigidBody.AddForce(new Vector3(0, 500, 0));
+    }
+}
+
+    
+
